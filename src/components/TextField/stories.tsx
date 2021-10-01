5776d8cac819afe5f56fe2077b08ea37f67a4382
Douglas Dantas <douglas.mdantas@hotmail.com>
2021-09-30 22:48:12 -0300

    Adicionando uma nova seção 'Form' para o storybook

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -3,7 +3,7 @@ import { Mail } from '@styled-icons/material-outlined'
 import TextField, { TextFieldProps } from '.'
 
 export default {
-  title: 'TextField',
+  title: 'Form/TextField',
   component: TextField,
   args: {
     label: 'E-mail',
